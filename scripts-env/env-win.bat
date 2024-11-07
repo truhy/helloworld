e535b0d513f03413fa98e5cc79289df10da0ed02
Truong Hy <truhy7@gmail.com>
2024-11-07 19:59:56 +0000

    Improved make scripts
    Enable spaces in parent folders

--- scripts-env/env-win.bat
+++ scripts-env/env-win.bat
@@ -11,9 +11,13 @@ SET OPENOCD_PATH=C:\devtools\xpack-openocd-0.12.0-2\bin
 
 :: Application settings
 SET APP_HOME_PATH=%SCRIPT_PATH%
-SET APP_OUT_PATH=%SCRIPT_PATH%
+SET APP_OUT_PATH=.
 SET APP_SRC_PATH1=source
 SET APP_PROGRAM_NAME1=helloworld
+
+:: Other stuff
+CALL :NORMALIZEPATH %APP_OUT_PATH%
+SET APP_OUT_FULL_PATH=%RETVAL%
 
 :: Search path settings
 SET path=%path%;%SCRIPT_PATH%\scripts-env;%SCRIPT_PATH%\scripts-win
@@ -25,3 +29,9 @@ IF "%OPENOCD_PATH%" NEQ "" SET path=%path%;%OPENOCD_PATH%
 IF "%MAKE_PATH%" NEQ ""      ECHO Make     : %MAKE_PATH%
 IF "%TOOLCHAIN_PATH%" NEQ "" ECHO Toolchain: %TOOLCHAIN_PATH%
 IF "%OPENOCD_PATH%" NEQ ""   ECHO OpenOCD  : %OPENOCD_PATH%
+
+EXIT /B
+
+:NORMALIZEPATH
+  SET RETVAL=%~f1
+  EXIT /B
